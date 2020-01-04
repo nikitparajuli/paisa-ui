019d904db055633c8c0c375c5516c7e4f08f86ee
nikitparajuli <nikitparajuli@yahoo.com>
2020-01-04 18:47:02 -0500

    add horizontal header and set app color

--- src/appLayout/AppHeader.tsx
+++ src/appLayout/AppHeader.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+
+import { Logo } from './';
 
 type AppHeaderProps = {};
 
 export const AppHeader: React.FC<AppHeaderProps> = () => {
     return (
-        <>
-        </>
+        <Row gutter={24} className="app-header">
+            <Col span={4} style={{paddingTop: 10}}>
+                <Logo />
+            </Col>
+        </Row>
     );
 }
